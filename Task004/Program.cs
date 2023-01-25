2dde15b161cb6bb0e37bc6cea1bef403a47741cb
Zosyja <zosyja@bk.ru>
2023-01-25 19:37:37 +0300

    added Task044

---
+++ Task004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+for (int i=number; number!=0; number=number/10)
+{
+    int last = number%10;
+    sum = sum + last;
+}
+Console.Write("Сумма цифр в числе = ");
+Console.WriteLine(sum);
