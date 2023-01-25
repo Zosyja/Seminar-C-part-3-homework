13161e7654071482992ab5ee3f3488a7e3ddcd72
Zosyja <zosyja@bk.ru>
2023-01-25 19:12:28 +0300

    added Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать сумму чисел от 1 до A
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+int number = 1;
+while(number<=A)
+{
+    sum = sum + number;
+    number++;
+}
+Console.Write("Сумма чисел от 1 до " + A + " = ");
+Console.Write(sum);
