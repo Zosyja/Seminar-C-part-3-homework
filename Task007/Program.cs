0fff4cf1c32261096458f6c3aeddd0ca162b1919
Zosyja <zosyja@bk.ru>
2023-01-25 22:37:44 +0300

    added Task007

---
+++ Task007/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void FillMas(int[] arr)
+{
+    for (int i=0; i<arr.Length; i++)
+    {
+        arr[i] = 0;
+    }
+}
+
+void PrintMas(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = new int[8];
+FillMas(arr);
+PrintMas(arr);
