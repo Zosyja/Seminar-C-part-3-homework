b40b51b60f5b6cc8cd5bcdbf0fea79381631ef12
Zosyja <zosyja@bk.ru>
2023-01-26 00:21:28 +0300

    added Task012

---
+++ Task012/Program.cs
@@ -0,0 +1,39 @@
+﻿// Найти произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+void FillMas(int[] arr)
+{
+    for (int i=0; i<arr.Length; i++)
+    {
+        int num = new Random().Next(10);
+        arr[i] = num;
+    }
+}
+void PrintMas(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+void MultiMass(int[] arr)
+{
+    int i = 0;
+    while (i < arr.Length/2)
+    {
+        arr[i] = arr[i] * arr[arr.Length-1-i];
+        arr[arr.Length-1-i] = 0;
+        i++;
+    }
+}
+
+int[] arr = new int[10];
+FillMas(arr);
+PrintMas(arr);
+MultiMass(arr);
+
+for (int el = 0; el < arr.Length/2; el++)
+{
+    Console.Write(arr[el] + " ");
+}
