6d7bcd79c285a03baea94b13bc27927c12465f32
Zosyja <zosyja@bk.ru>
2023-01-25 19:11:12 +0300

    added Task 001

---
+++ Task001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 1; i < number+1; i++)
+{
+    Console.Write(i + ": ");
+    int cube = i*i*i;
+    Console.WriteLine(cube);
+}
