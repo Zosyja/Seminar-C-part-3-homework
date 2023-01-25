8aa7dd17c403186c8e681ba18b6d57548bae7d51
Zosyja <zosyja@bk.ru>
2023-01-25 19:24:00 +0300

    added Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,12 @@
+﻿// Возведите число в натуральную степень В используя цикл
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число B (натуральная степень): ");
+int B = int.Parse(Console.ReadLine() ?? "0");
+int number = A;
+for (int i=2; i<=B; i++)
+{
+    number = number * A;
+}
+Console.Write("Число " + A + " в степени " + B + " = ");
+Console.Write(number);
