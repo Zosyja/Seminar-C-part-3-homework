acb321020d4389260785da4f94e67c4cf6975cd9
Zosyja <zosyja@bk.ru>
2023-01-25 23:52:49 +0300

    added Task011

---
+++ Task011/Program.cs
@@ -0,0 +1,34 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+void FillMas(int[] arr)
+{
+    for (int i=0; i<arr.Length; i++)
+    {
+        int num = new Random().Next(3);
+        arr[i] = num;
+    }
+}
+void PrintMas(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+int Sum(int[] arr)
+{
+    int result = 0;
+    int i = 1;
+    while(i <= arr.Length)
+    {
+        result = result + arr[i];
+        i = i + 2;
+    }
+    return result;
+}
+
+int[] arr = new int[10];
+FillMas(arr);
+PrintMas(arr);
+Console.Write("Сумма чисел массива стоящих на нечетной позиции: ");
+Console.WriteLine(Sum(arr));
