0ea69dd464c0f49ec65aec9e69195abd9f39a96a
Zosyja <zosyja@bk.ru>
2023-01-25 22:11:52 +0300

    added Task005

---
+++ Task005/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+int result = 1;
+while (i <= number)
+{
+    result = result * i;
+    i++;
+}
+Console.Write("Произведение чисел от 1 до " + number + " = ");
+Console.Write(result);
